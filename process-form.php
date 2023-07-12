296b7065dacadad0bce82da79c44713ebaee94ef
HrudhaiUmas <hrudhaiumas20@gmail.com>
2023-07-13 00:04:08 +0530

    starting backend

---
+++ process-form.php
@@ -0,0 +1,25 @@
+<?php
+
+$majors = $_POST["majors"];
+$admitted1 = $_POST["Admitted1"];
+$dual = $_POST["dual"];
+$majors2 = $_POST["majors2"];
+$wsa = filter_input(INPUT_POST, "Wsa", FILTER_VALIDATE_FLOAT);
+$standing = $_POST["standing"];
+$resident = $_POST["resident"];
+$firstgen = $_POST["firstgen"];
+$running = $_POST["running"];
+$transfer = $_POST["transfer"];
+$transcredit = $_POST["transcredit"];
+$internship = $_POST["internship"];
+$work = $_POST["work"];
+$gpa = filter_input(INPUT_POST, "gpa", FILTER_VALIDATE_FLOAT);
+$coursestaken = filter_input(INPUT_POST, "coursestaken", FILTER_VALIDATE_INT);
+$firstapp = $_POST["firstapp"];
+$message = $_POST["message"];
+
+
+var_dump($majors, $admitted1, $dual, $majors2, $wsa, $standing,
+$resident, $firstgen, $running, $transfer ,$transcredit ,$internship, 
+$work , $gpa, $coursestaken, $firstapp, $message);
+?>
